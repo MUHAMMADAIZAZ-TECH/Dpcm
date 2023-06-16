39a23be37d68892d88643682eeec92bed69d7ce1
MUHAMMADAIZAZ-TECH <aizaznadeem174@gmail.com>
2023-06-16 07:10:47 +0500

    build changes

--- Server/src/routes/treatmentPlan.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const treatmentPlanController = require("../controllers/treatmentPlan");
-
-// Define routes
-router.post("/", treatmentPlanController.createTreatmentPlan);
-// router.post('/', treatmentPlanController.addPrice);
-
-module.exports = router;
