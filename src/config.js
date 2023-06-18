210c6306e3e62b9b21c071cd5f1125909944448d
MUHAMMADAIZAZ-TECH <aizaznadeem174@gmail.com>
2023-06-18 16:27:20 +0500

    last final chages

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// export const Config = "https://lionfish-app-bmtdd.ondigitalocean.app/";
-export const Config = "http://localhost:3000/";
+export const Config = "https://lionfish-app-bmtdd.ondigitalocean.app/";
